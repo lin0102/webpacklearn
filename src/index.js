dc2f216ae432a59ef76bfa4db92764df2f5ef5e7
lin0102 <1262280723@qq.com>
2018-05-30 17:08:44 +0800

    second commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Rose from './img/rose.jpg'
+import './index.css'
+// ReactDOM.render(<div>hello webpack</div>,document.body);
+class App extends React.Component {
+    state  ={
+        reset :'yes'
+    }
+    onClick = () => {
+        this.setState({
+            reset: this.state.reset === 'yes' ? 'no' : 'yes'
+        })
+    }
+    render () {
+        return (
+            <div><img src = {Rose} alt="玫瑰" className = {this.state.reset === 'yes' ? 'flower' : 'flower flower--rotate'} onClick = {this.onClick} /></div>
+        )
+    }
+}
+ReactDOM.render(<App />,document.body);
